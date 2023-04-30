bc6feda741c2256e9c78dd52b8f76e3066ac27e8
airatB <79276775026@yandex.ru>
2023-04-30 17:50:17 +0300

    fix: describe main models

--- models/shorten.go
+++ models/shorten.go
@@ -1,29 +1,29 @@
-// Package models describe models.
+// Package models describes main entities.
 package models
 
-// ShortenRequest instance
+// ShortenRequest describes request fields
 type ShortenRequest struct {
 	URL string `json:"url"`
 }
 
-// ShortenResponse instance
+// ShortenResponse describes response fields
 type ShortenResponse struct {
 	Result string `json:"result"`
 }
 
-// URLResponse instance
+// URLResponse describes URL response fields
 type URLResponse struct {
 	ShortURL    string `json:"short_url"`
 	OriginalURL string `json:"original_url"`
 }
 
-// BatchShortenRequest instance
+// BatchShortenRequest describes request fields when we save batch
 type BatchShortenRequest struct {
 	CorrelationID string `json:"correlation_id"`
 	OriginalURL   string `json:"original_url"`
 }
 
-// BatchShortenResponse instance
+// BatchShortenResponse describes response fields when we save batch
 type BatchShortenResponse struct {
 	CorrelationID string `json:"correlation_id"`
 	ShortURL      string `json:"short_url"`
